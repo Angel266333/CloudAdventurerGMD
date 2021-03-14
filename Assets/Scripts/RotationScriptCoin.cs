31069b092cdada85324bb4412071e9d70b8e9704
ANG3L <angelilianov@gmail.com>
2021-03-14 17:25:56 +0100

    Prefab for coins - placing on clouds and rotation script for the coins.

---
+++ Assets/Scripts/RotationScriptCoin.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotationScriptCoin : MonoBehaviour
+{
+    // Tutorial used: www.youtube.com/watch?v=AIcDUqFcwX4 - Unity3D - Rotating Item
+    // Update is called once per frame
+    float rotationSpeed = 50.0f;
+    void Update()
+    {
+        transform.Rotate(new Vector3(Time.deltaTime * rotationSpeed, 0, 0));
+    }
+}
