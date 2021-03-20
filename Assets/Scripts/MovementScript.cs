c8a88be78f5b65f78a64f51079134c2cb7799810
ANG3L <angelilianov@gmail.com>
2021-03-20 12:26:16 +0100

    Added key prefab, made movement script for clouds

---
+++ Assets/Scripts/MovementScript.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementScript : MonoBehaviour
+{
+    /* Credits for script code - compilation of two scripts:
+     * https://forum.unity.com/posts/5931503
+     * https://answers.unity.com/questions/754633/how-to-move-an-object-left-and-righ-looping.html
+     */
+
+    //adjust this to change speed
+    [SerializeField] float vSpeed = 5f;
+    [SerializeField] float xSpeed = 5f;
+
+    //adjust this to change how high it goes
+    [SerializeField] float height = 25f;
+
+    Vector3 pos;
+
+    public bool vMovement = true;
+
+    public bool xMovement = true;
+
+    private void Start()
+    {
+        pos = transform.position;
+    }
+
+    void Update()
+    {
+        if (xMovement)
+        {
+            Vector3 v = pos;
+            v.x += xSpeed * Mathf.Sin (Time.time * xSpeed);
+            transform.position = v;
+        }
+        if (vMovement) {
+//calculate what the new Y position will be
+            float newY = Mathf.Sin(Time.time * vSpeed) * height + pos.y;
+//set the object's Y to the new calculated Y
+            transform.position = new Vector3(transform.position.x, newY, transform.position.z);
+        }
+
+    }
+}
--- README.md
+++ README.md
@@ -1,2 +1,21 @@
 # CloudAdventurerGMD
 Repository for the GMD course assignment
+
+Project made with Unity version 2019.4.21f1
+--------------------------------------------------------------------------------------------
+Used Assets:
+
+CastlePack: 
+https://assetstore.unity.com/packages/3d/environments/castle-pack-by-progru-185976
+
+RustKey: 
+https://assetstore.unity.com/packages/3d/props/rust-key-167590
+--------------------------------------------------------------------------------------------
+Script source code credits:
+
+RotationScriptCoin: 
+https://www.youtube.com/watch?v=AIcDUqFcwX4/
+
+MovementScript: 
+https://forum.unity.com/posts/5931503
+https://answers.unity.com/questions/754633/how-to-move-an-object-left-and-righ-looping.html
