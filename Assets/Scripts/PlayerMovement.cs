5a2e73eaf0c3cd59716e9bd6d7ff41897f600a57
ANG3L <angelilianov@gmail.com>
2021-03-29 18:12:56 +0200

    Improve player movement and added jump functionality

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -8,13 +8,30 @@ public class PlayerMovement : MonoBehaviour
     public CharacterController controller;
 
     public float speed = 12;
+    public float gravity = -9.81f;
+    public float jumpHeight = 3f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.4f;
+    public LayerMask groundMask;
     
     private float x;
     private float z;
+
+    private Vector3 velocity;
+    private bool isGrounded;
     
     // Update is called once per frame
     void Update()
     {
+
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+        
         x = Input.GetAxis("Horizontal");
         z = Input.GetAxis("Vertical");
 
@@ -22,5 +39,13 @@ public class PlayerMovement : MonoBehaviour
 
         controller.Move(move * speed * Time.deltaTime);
 
+        if (Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+
+            velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
     }
 }
