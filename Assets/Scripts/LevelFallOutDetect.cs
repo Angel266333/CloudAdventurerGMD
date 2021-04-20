c8bf7f83840203f87b6a5d64f5d68690b50db1bd
ANG3L <angelilianov@gmail.com>
2021-04-20 13:30:27 +0200

    When player falls through level, the game will return to main menu

---
+++ Assets/Scripts/LevelFallOutDetect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelFallOutDetect : MonoBehaviour
+{
+
+
+    public Transform player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        gameObject.GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (player.transform.position.y < -5f)
+        {
+            SceneManager.LoadScene(0);
+        }  
+    }
+}
