6c62768122d48ba53fa6411a8eb1d276a99ddbf2
ANG3L <angelilianov@gmail.com>
2021-05-15 12:55:18 +0200

    Added Victory screen when player finished game on the third level

---
+++ Assets/Scripts/Victory.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Victory : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.name == "Player")
+        {
+            Destroy(gameObject); // destroy diamond
+            SceneManager.LoadScene("Victory"); // Takes you to victory level.
+        }
+            
+    }
+}
